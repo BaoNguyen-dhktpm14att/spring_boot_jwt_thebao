d1f62999438cc87d3b781a42978e96efc04bce52
DESKTOP-1VFJKRB\USER <ngtbao2000@gmail.com>
2021-09-24 08:45:12 +0700

    finish entity trong branch master

---
+++ src/test/java/com/example/spring_boot_jwt_bao/SpringBootJwtBaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_bao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtBaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
